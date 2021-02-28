e81888f1b06996074235097ba6250ef416095cd3
iano osborne <ianosborne@ians-imac.attlocal.net>
2021-02-28 13:19:29 -0800

    updated

--- Script.js
+++ Script.js
@@ -1,26 +1,67 @@
 var startButton = document.getElementById("startbutton");
 var ticker = document.getElementById("timer");
-var timeLeft = 60;
-var timerElement
-var questions = { question: "What is a boolean?", choices: ["an element with quotations", "a number", "a true or false"], answer: "a true or false" }
+var timeLeft = 20;
+var timerElement;
+var score = 0;
+var questions = [
+  {
+    question: "What is a boolean?",
+    choices: ["an element with quotations", "a number", "a true or false"],
+    answer: "a true or false",
+  },
+  {
+    question: "what is most used language?",
+    choices: ["Javascript", "Python", "Ruby"],
+    answer: "Javascript",
+  },
+];
+var currentquestions = 0;
+var questionElement = document.getElementById("questions");
+var buttonElement1 = document.getElementById("answers1");
+var buttonElement2 = document.getElementById("answers2");
+var buttonElement3 = document.getElementById("answers3");
+buttonElement1.addEventListener("click", function (event) {
+  console.log(event.target.innerText);
+  if (event.target.innerText === questions[currentquestions].answer) {
+    score++;
+    console.log(currentquestions);
+    currentquestions++;
+  } else {
+    timeLeft = timeLeft - 5;
+    console.log(timeLeft);
+  }
+});
 
 startButton.addEventListener("click", function () {
-    startquiz()
+  startquiz();
 });
 
 function timer() {
-    timeLeft--
-    ticker.textContent = timeLeft
-    if (timeLeft == 0) {
-        clearInterval(myInterval)
-    }
+  timeLeft--;
+  ticker.textContent = timeLeft;
+  if (timeLeft == 0) {
+    clearInterval(myInterval);
+  }
 }
 
 let myInterval; // declare as global
 
 function startquiz() {
-    // assign myInterval and start time
-    return myInterval = setInterval(function () { timer() }, 1000);
+  console.log(questions[currentquestions].choices);
+  console.log(questions[currentquestions].answer);
+  console.log(questions[currentquestions].question);
+  displayQuestion();
+  return (myInterval = setInterval(function () {
+    timer();
+  }, 1000));
 }
-
-function questions
+function displayQuestion() {
+  questionElement.textContent = questions[currentquestions].question;
+  buttonElement1.textContent = questions[currentquestions].choices[0];
+  buttonElement2.textContent = questions[currentquestions].choices[1];
+  buttonElement3.textContent = questions[currentquestions].choices[2];
+}
+// put the question on the page
+// on click answer
+// grade
+// reassign current question to the next one
