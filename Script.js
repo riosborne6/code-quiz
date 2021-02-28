d3032415cdb6debda9387531bf1df32d720e1a40
iano osborne <ianosborne@ians-imac.attlocal.net>
2021-02-28 14:34:43 -0800

    updated brackets

--- Script.js
+++ Script.js
@@ -30,23 +30,25 @@ buttonElement1.addEventListener("click", function (event) {
     timeLeft = timeLeft - 5;
     console.log(timeLeft);
   }
-  buttonElement2.addEventListener("click", function (event) {
-    console.log(event.target.innerText);
-    if (event.target.innerText === questions[currentquestions].answer) {
-      score++;
-      console.log(currentquestions);
-      currentquestions++;
-    } else {
-      timeLeft = timeLeft - 5;
-      console.log(timeLeft);
-    }
-    buttonElement3.addEventListener("click", function (event) {
-        console.log(event.target.innerText);
-        if (event.target.innerText === questions[currentquestions].answer) {
-          score++;
-          console.log(currentquestions);
-          currentquestions++;
-        }
+});
+buttonElement2.addEventListener("click", function (event) {
+  console.log(event.target.innerText);
+  if (event.target.innerText === questions[currentquestions].answer) {
+    score++;
+    console.log(currentquestions);
+    currentquestions++;
+  } else {
+    timeLeft = timeLeft - 5;
+    console.log(timeLeft);
+  }
+});
+buttonElement3.addEventListener("click", function (event) {
+  console.log(event.target.innerText);
+  if (event.target.innerText === questions[currentquestions].answer) {
+    score++;
+    console.log(currentquestions);
+    currentquestions++;
+  }
 });
 
 startButton.addEventListener("click", function () {
