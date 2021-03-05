d7c5693e55c9329ac20594b62fc39c02d6d19247
iano osborne <ianosborne@ians-imac.attlocal.net>
2021-03-04 19:19:49 -0800

    thursday evening

--- Script.js
+++ Script.js
@@ -17,49 +17,6 @@ var questions = [
   },
 ];
 var currentquestions = 0;
-var questionElement = document.getElementById("questions");
-var buttonElement1 = document.getElementById("answers1");
-var buttonElement2 = document.getElementById("answers2");
-var buttonElement3 = document.getElementById("answers3");
-buttonElement1.addEventListener("click", function (event) {
-  console.log(event.target.innerText);
-  submitAnswer(event.target.innerText);
-  // if (event.target.innerText === questions[currentquestions].answer) {
-  //   score++;
-  //   console.log(currentquestions);
-  //   currentquestions++;
-  // } else {
-  //   timeLeft = timeLeft - 5;
-  //   console.log(timeLeft);
-  //   currentquestions++;
-  // }
-});
-buttonElement2.addEventListener("click", function (event) {
-  submitAnswer(event.target.innerText);
-  console.log(event.target.innerText);
-  // if (event.target.innerText === questions[currentquestions].answer) {
-  //   score++;
-  //   console.log(currentquestions);
-  //   currentquestions++;
-  // } else {
-  //   timeLeft = timeLeft - 5;
-  //   console.log(timeLeft);
-  //   currentquestions++;
-  // }
-});
-buttonElement3.addEventListener("click", function (event) {
-  submitAnswer(event.target.innerText);
-  console.log(event.target.innerText);
-  // if (event.target.innerText === questions[currentquestions].answer) {
-  //   score++;
-  //   console.log(currentquestions);
-  //   currentquestions++;
-  // } else {
-  //   timeLeft = timeLeft - 5;
-  //   console.log(timeLeft);
-  //   currentquestions++;
-  // }
-});
 
 startButton.addEventListener("click", function () {
   startquiz();
@@ -76,6 +33,18 @@ function timer() {
 let myInterval; // declare as global
 
 function startquiz() {
+  var questionEl = document.createElement("h3");
+  questionEl.setAttribute("id", "questions");
+  var buttonEl1 = document.createElement("button");
+  buttonEl1.setAttribute("id", "answers1");
+  var buttonEl2 = document.createElement("button");
+  buttonEl2.setAttribute("id", "answers2");
+  var buttonEl3 = document.createElement("button");
+  buttonEl3.setAttribute("id", "answers3");
+  quizEl.appendChild(questionEl);
+  quizEl.appendChild(buttonEl1);
+  quizEl.appendChild(buttonEl2);
+  quizEl.appendChild(buttonEl3);
   console.log(questions[currentquestions].choices);
   console.log(questions[currentquestions].answer);
   console.log(questions[currentquestions].question);
@@ -85,10 +54,54 @@ function startquiz() {
   }, 1000));
 }
 function displayQuestion() {
+  var questionElement = document.getElementById("questions");
+  var buttonElement1 = document.getElementById("answers1");
+  var buttonElement2 = document.getElementById("answers2");
+  var buttonElement3 = document.getElementById("answers3");
+  console.log(questions);
   questionElement.textContent = questions[currentquestions].question;
   buttonElement1.textContent = questions[currentquestions].choices[0];
   buttonElement2.textContent = questions[currentquestions].choices[1];
   buttonElement3.textContent = questions[currentquestions].choices[2];
+  buttonElement1.addEventListener("click", function (event) {
+    console.log(event.target.innerText);
+    submitAnswer(event.target.innerText);
+    // if (event.target.innerText === questions[currentquestions].answer) {
+    //   score++;
+    //   console.log(currentquestions);
+    //   currentquestions++;
+    // } else {
+    //   timeLeft = timeLeft - 5;
+    //   console.log(timeLeft);
+    //   currentquestions++;
+    // }
+  });
+  buttonElement2.addEventListener("click", function (event) {
+    submitAnswer(event.target.innerText);
+    console.log(event.target.innerText);
+    // if (event.target.innerText === questions[currentquestions].answer) {
+    //   score++;
+    //   console.log(currentquestions);
+    //   currentquestions++;
+    // } else {
+    //   timeLeft = timeLeft - 5;
+    //   console.log(timeLeft);
+    //   currentquestions++;
+    // }
+  });
+  buttonElement3.addEventListener("click", function (event) {
+    submitAnswer(event.target.innerText);
+    console.log(event.target.innerText);
+    // if (event.target.innerText === questions[currentquestions].answer) {
+    //   score++;
+    //   console.log(currentquestions);
+    //   currentquestions++;
+    // } else {
+    //   timeLeft = timeLeft - 5;
+    //   console.log(timeLeft);
+    //   currentquestions++;
+    // }
+  });
 }
 
 function submitAnswer(answer) {
@@ -102,14 +115,24 @@ function submitAnswer(answer) {
   console.log(currentquestions, questions.length - 1);
   if (currentquestions > questions.length - 1) {
     time = 0;
-    clearQuestions();
+    clearQuestions(
+      questionElement,
+      buttonElement1,
+      buttonElement2,
+      buttonElement3
+    );
     finishQuiz();
     return;
   }
   console.log(answer);
   displayQuestion();
 }
-function clearQuestions() {
+function clearQuestions(
+  questionElement,
+  buttonElement1,
+  buttonElement2,
+  buttonElement3
+) {
   quizEl.removeChild(questionElement);
   quizEl.removeChild(buttonElement1);
   quizEl.removeChild(buttonElement2);
@@ -117,12 +140,35 @@ function clearQuestions() {
 }
 function finishQuiz() {
   var h1El = document.createElement("h1");
+  var inputEl = document.createElement("input");
+  var buttonEl = document.createElement("button");
+  buttonEl.setAttribute("id", "inputButton");
+  inputEl.setAttribute("id", "initialsInput");
+  buttonEl.textContent = "submit your score";
   h1El.textContent = score;
   //input element. Give it an ID. Create a submit button. Give submit button and onclick event - set attribute.
   //submit button should should set local storage
   quizEl.appendChild(h1El);
+  quizEl.appendChild(inputEl);
+  quizEl.appendChild(buttonEl);
+  var buttonClick = document.getElementById("inputButton");
+  buttonClick.addEventListener("click", function () {
+    console.log("itworks");
+    var initialClick = document.getElementById("initialsInput").value;
+    var scoreObject = {
+      score: score,
+      initials: initialClick,
+    };
+    localStorage.setItem("quizScore", JSON.stringify(scoreObject));
+    showHighScores(h1El, inputEl, buttonEl);
+  });
 }
-function showHighScores() {
-  // get local storage.
+function showHighScores(h1El, inputEl, buttonEl) {
+  var score = JSON.parse(localStorage.getItem("quizScore"));
+  quizEl.removeChild(h1El);
+  quizEl.removeChild(inputEl);
+  quizEl.removeChild(buttonEl);
+  currentquestions = 0;
+  console.log(score);
   // set the text content from the get local storage (html elements).
 }
